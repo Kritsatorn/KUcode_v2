17f8b220f01416fccbdc6a713c75e81203871a64
Kritsatorn <32409782+Kritsatorn@users.noreply.github.com>
2022-02-23 12:50:47 +0700

    Updated: Learning Ouline & DnD iFrame

--- web/src/components/DnDIframe/DnDIframe.js
+++ web/src/components/DnDIframe/DnDIframe.js
@@ -1,8 +1,59 @@
-const DnDIframe = () => {
+import { Rnd } from 'react-rnd'
+import { FaCircle } from 'react-icons/fa'
+
+const style = {
+  overflow: 'hidden',
+  overflowX: 'hidden',
+  border: 'solid 1px #ddd',
+  background: 'gray',
+  zIndex: 2,
+  borderRadius: '4px',
+}
+const initialCode = `
+  <!DOCTYPE html>
+  <html>
+    <body>
+
+      <h1>My First Heading</h1>
+      <p>My first paragraph.</p>
+
+    </body>
+  </html>
+`
+
+const DnDIframe = ({ compiledCode = initialCode }) => {
   return (
     <div>
-      <h2>{'DnDIframe'}</h2>
-      <p>{'Find me in ./web/src/components/DnDIframe/DnDIframe.js'}</p>
+      <Rnd
+        style={style}
+        default={{
+          x: 1100,
+          y: 100,
+          width: 320,
+          height: 350,
+        }}
+      >
+        <div className="iframe-headbar w-full h-8 fixed left-0 top-0 bg-skin-headIframe pl-2 text-sm flex items-center ">
+          <div className="btn-iframe btn-close mx-1 ">
+            <FaCircle />
+          </div>
+          <div className="btn-iframe btn-min mx-1">
+            <FaCircle />
+          </div>
+          <div className="btn-iframe btn-max mx-1">
+            <FaCircle />
+          </div>
+        </div>
+        <iframe
+          className="pt-8"
+          srcDoc={compiledCode}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="calc(100% )"
+        />
+      </Rnd>
     </div>
   )
 }
--- web/src/components/Learning/Learning.js
+++ web/src/components/Learning/Learning.js
@@ -1,8 +1,41 @@
+import AudioPlayer from 'src/components/AudioPlayer'
+import DnDIframe from 'src/components/DnDIframe'
+import CodeEditer from 'src/components/CodeEditer'
+import SideBar from 'src/components/SideBar'
 const Learning = () => {
   return (
     <div>
-      <h2>{'Learning'}</h2>
-      <p>{'Find me in ./web/src/components/Learning/Learning.js'}</p>
+      <DnDIframe
+      // code={srcDoc}
+      />
+      <div className=" relative flex w-full h-full  ">
+        <div className="  w-48 h-full box-content ">
+          <SideBar />
+        </div>
+        <div className=" bg-skin-editerDark w-full h-full box-border">
+          <CodeEditer
+          // theme={theme}
+          // language={language}
+          // code={code}
+          // handleEditorDidMount={handleEditorDidMount}
+          // handleEditorChange={handleEditorChange}
+          />
+        </div>
+      </div>
+      <div className="z-10 left-0 bottom-0 fixed w-full h-10 ">
+        <AudioPlayer
+        // onPlayFn={() => {
+        //   let { currentTime } = audioRef.current
+        //   replayTyping(currentTime)
+        // }}
+        // onPause={() => {
+        //   clearTimeout(timeID.current)
+        //   timeID.current = null
+        // }}
+        // audioRef={audioRef}
+        // audioURL={soundUrl}
+        />
+      </div>
     </div>
   )
 }
