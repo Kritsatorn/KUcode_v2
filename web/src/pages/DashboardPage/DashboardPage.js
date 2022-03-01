2714288f8cdaa4e379b501890c269eb966dcc907
Kritsatorn <32409782+Kritsatorn@users.noreply.github.com>
2022-03-01 10:23:05 +0700

    Upadted: organize file

--- web/src/pages/DashboardPage/DashboardPage.js
+++ web/src/pages/DashboardPage/DashboardPage.js
@@ -1,8 +1,6 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-import LearningsCell from 'src/components/LearningsCell'
 import ImagesCell from 'src/components/Image/ImagesCell'
-import TeacherSlideCell from 'src/components/TeacherSlideCell'
 const DashboardPage = () => {
   return (
     <>
@@ -10,7 +8,7 @@ const DashboardPage = () => {
 
       <div className=" w-full h-full bg-slate-400">
         {/* <LearningsCell /> */}
-        <TeacherSlideCell kuy="k" hee="h" />
+
         {/* <Link to={routes.article({ id: article.id })}>{article.title}</Link> */}
         <ImagesCell />
         <Link to={routes.learningRecord({ imageIDList: [1, 2, 3] })}>
