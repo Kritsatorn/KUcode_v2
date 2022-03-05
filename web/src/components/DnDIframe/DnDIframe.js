f40510b2d6bf551835cbb454e94c085a1b68e39e
Kritsatorn <32409782+Kritsatorn@users.noreply.github.com>
2022-03-05 15:45:07 +0700

    Added: change bg iframe

--- web/src/components/DnDIframe/DnDIframe.js
+++ web/src/components/DnDIframe/DnDIframe.js
@@ -63,13 +63,13 @@ const DnDIframe = ({ compiledCode = initialCode, isOpen, setClose }) => {
           </button>
         </div>
         <iframe
-          className="pt-8"
+          className="pt-8 bg-slate-600 "
           srcDoc={compiledCode}
           title="output"
           sandbox="allow-scripts"
           frameBorder="0"
           width="100%"
-          height="calc(100%)"
+          height="100%"
         />
       </Rnd>
     </div>
