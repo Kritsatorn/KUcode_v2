571f2318fad3facbbbf1350ee4db2a90714c1eb3
Kritsatorn <32409782+Kritsatorn@users.noreply.github.com>
2022-03-06 16:35:46 +0700

    Del: trash

--- web/src/pages/DashboardPage/DashboardPage.js
+++ web/src/pages/DashboardPage/DashboardPage.js
@@ -1,19 +1,13 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import { useState } from 'react'
-import ImagesCell from 'src/components/Image/ImagesCell'
 import NewLearning from 'src/components/NewLearning'
 const DashboardPage = () => {
-  const [listImg, setListImg] = useState([])
   return (
     <>
       <MetaTags title="Dashboard" description="Dashboard page" />
 
       <div className=" w-full h-full bg-slate-400 flex flex-col">
-        {/* <ImagesCell />
-        <Link to={routes.learningRecord({ imageIDList: [1, 2, 3] })}>
-          to Record
-        </Link> */}
         <div className="bg-blue-500 flex-1 flex">
           <div className="bg-red-400  flex-1 h-full">SIDEbAR</div>
           <div className=" bg-purple-500 w-5/6 ">
